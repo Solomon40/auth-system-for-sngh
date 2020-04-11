e8c623bdd226edb81f4acc26743c2737a43f4d14
Tochukwu Solomon Ewiwilem <tochukwuewiwilem@gmail.com>
2020-04-11 14:59:12 +0100

    fixed success message

--- contact/processcontact.php
+++ contact/processcontact.php
@@ -1,8 +1,9 @@
 <?php 
+session_start();
 
 
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'] ;
+$firstname = $_POST['first_name'];
+$lastname = $_POST['last_name'] ;
 $email = $_POST['email'];
 $gender = $_POST['gender'];
 $track = $_POST['track'];
@@ -15,7 +16,7 @@ $user_object = [
             'track'=>$track
 ];
 
-file_put_contents("contact/" . $firstname . $lastname . ".json", json_encode($user_object));
+file_put_contents($firstname . $lastname . ".json", json_encode($user_object));
         $_SESSION['message'] = "Response recieved, " . $firstname . "!";
         header("Location: index.php ");
 
